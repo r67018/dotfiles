b38785d432b47f7be5e187dc2d9cb8944e8cada9
swk67018 <swear1208a13@gmail.com>
2022-12-24 19:33:24 +0900

    Quit telescope on first esc

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -1,7 +1,13 @@
 local telescope = require('telescope')
-
+local actions = require('telescope.actions')
 telescope.setup {
-
+    defaults = {
+        mappings = {
+            i = {
+                ['<esc>'] = actions.close,
+            }
+        }
+    }
 }
 
 telescope.load_extension 'fzf'
