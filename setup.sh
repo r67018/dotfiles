bb17c296f755aa16ae122b863faf8812bf16ae4a
Ryosei Goto <swear1208a13@gmail.com>
2024-05-29 11:41:00 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 PWD=$(pwd -P)
 
 ln -nfs $PWD/config/tmux/.tmux.conf ~/.tmux.conf
@@ -5,3 +7,5 @@ ln -nfs $PWD/config/fish ~/.config/fish
 ln -nfs $PWD/config/nvim ~/.config/nvim
 ln -nfs $PWD/config/intellij/.ideavimrc ~/.ideavimrc
 ln -nfs $PWD/config/wezterm/.wezterm.lua ~/.wezterm.lua
+dconf load /org/gnome/terminal/ < $PWD/config/gnome/terminal/settings.txt
+
