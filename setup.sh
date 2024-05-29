44d8c2454dfc1b864b01b437f3e33a22bdfe17dd
Ryosei Goto <swear1208a13@gmail.com>
2024-05-29 16:59:35 +0900

    Rename config to .config

--- setup.sh
+++ setup.sh
@@ -5,19 +5,19 @@ PWD=$(pwd -P)
 # zsh
 ln -nfs $PWD/.zshrc ~/.zshrc
 # tmux
-ln -nfs $PWD/config/tmux/.tmux.conf ~/.tmux.conf
+ln -nfs $PWD/.config/tmux/.tmux.conf ~/.tmux.conf
 # fish
-ln -nfs $PWD/config/fish ~/.config/fish
+ln -nfs $PWD/.config/fish ~/.config/fish
 # git
-ln -nfs $PWD/config/git/.gitconfig ~/.gitconfig
+ln -nfs $PWD/.config/git/.gitconfig ~/.gitconfig
 # neovim
-ln -nfs $PWD/config/nvim ~/.config/nvim
+ln -nfs $PWD/.config/nvim ~/.config/nvim
 # IdeaVim(vim plugin for Jetbrains IDE)
-ln -nfs $PWD/config/intellij/.ideavimrc ~/.ideavimrc
+ln -nfs $PWD/.config/intellij/.ideavimrc ~/.ideavimrc
 # wezterm
-ln -nfs $PWD/config/wezterm/.wezterm.lua ~/.wezterm.lua
+ln -nfs $PWD/.config/wezterm/.wezterm.lua ~/.wezterm.lua
 # gnome terminal
 if command -v dconf &> /dev/null; then
-    dconf load /org/gnome/terminal/ < $PWD/config/gnome/terminal/settings.txt
+    dconf load /org/gnome/terminal/ < $PWD/.config/gnome/terminal/settings.txt
 fi
 
