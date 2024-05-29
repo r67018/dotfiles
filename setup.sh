481fcccfc1266094799040c70af14c67086d1d9c
Ryosei Goto <swear1208a13@gmail.com>
2024-05-29 11:58:18 +0900

    Add comment

--- setup.sh
+++ setup.sh
@@ -2,10 +2,15 @@
 
 PWD=$(pwd -P)
 
+# tmux
 ln -nfs $PWD/config/tmux/.tmux.conf ~/.tmux.conf
+# fish
 ln -nfs $PWD/config/fish ~/.config/fish
+# neovim
 ln -nfs $PWD/config/nvim ~/.config/nvim
+# IdeaVim(vim plugin for Jetbrains IDE)
 ln -nfs $PWD/config/intellij/.ideavimrc ~/.ideavimrc
+# wezterm
 ln -nfs $PWD/config/wezterm/.wezterm.lua ~/.wezterm.lua
 # gnome terminal
 if command -v dconf &> /dev/null; then
