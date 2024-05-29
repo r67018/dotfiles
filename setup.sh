d563937880d9637c6881c3f99f994b8f2250b97a
Ryosei Goto <swear1208a13@gmail.com>
2024-05-29 11:58:06 +0900

    Update setting up of gnome terminal

--- setup.sh
+++ setup.sh
@@ -7,5 +7,8 @@ ln -nfs $PWD/config/fish ~/.config/fish
 ln -nfs $PWD/config/nvim ~/.config/nvim
 ln -nfs $PWD/config/intellij/.ideavimrc ~/.ideavimrc
 ln -nfs $PWD/config/wezterm/.wezterm.lua ~/.wezterm.lua
-dconf load /org/gnome/terminal/ < $PWD/config/gnome/terminal/settings.txt
+# gnome terminal
+if command -v dconf &> /dev/null; then
+    dconf load /org/gnome/terminal/ < $PWD/config/gnome/terminal/settings.txt
+fi
 
