1d4ccaf7e48e87093b01d0f7d6c40cb4a9f85cd4
Ryosei Goto <swear1208a13@gmail.com>
2024-09-01 02:56:37 +0900

    Update Powershell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,10 @@
+# Set encoding to UTF-8
+[Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding('utf-8')
+
+# Variables
+$DHOME = "D:/Users/swear/"
+
+# Keybindings
 Set-PSReadLineKeyHandler -Key "Ctrl+d" -Function DeleteChar
 Set-PSReadLineKeyHandler -Key "Ctrl+w" -Function BackwardKillWord
 Set-PSReadLineKeyHandler -Key "Ctrl+u" -Function BackwardDeleteLine
@@ -10,3 +17,8 @@ Set-PSReadLineKeyHandler -Key "Alt+f" -Function NextWord
 Set-PSReadLineKeyHandler -Key "Alt+b" -Function BackwardWord
 Set-PSReadLineKeyHandler -Key "Ctrl+p" -Function PreviousHistory
 Set-PSReadLineKeyHandler -Key "Ctrl+n" -Function NextHistory
+
+#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+#f45873b3-b655-43a6-b217-97c00aa0db58
