83fcea13bf93f6c1c04fa801f3f6c46aad875d50
Ryosei Goto <swear1208a13@gmail.com>
2025-04-29 04:00:02 +0900

    Update

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,13 @@
+# Set encoding to UTF-8
+[Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding('utf-8')
+
+# Variables
+$DHOME = "D:/Users/swear/"
+
+# Aliases
+Set-Alias -Name vim -Value nvim
+
+# Keybindings
 Set-PSReadLineKeyHandler -Key "Ctrl+d" -Function DeleteChar
 Set-PSReadLineKeyHandler -Key "Ctrl+w" -Function BackwardKillWord
 Set-PSReadLineKeyHandler -Key "Ctrl+u" -Function BackwardDeleteLine
@@ -10,3 +20,10 @@ Set-PSReadLineKeyHandler -Key "Alt+f" -Function NextWord
 Set-PSReadLineKeyHandler -Key "Alt+b" -Function BackwardWord
 Set-PSReadLineKeyHandler -Key "Ctrl+p" -Function PreviousHistory
 Set-PSReadLineKeyHandler -Key "Ctrl+n" -Function NextHistory
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+#f45873b3-b655-43a6-b217-97c00aa0db58
+
