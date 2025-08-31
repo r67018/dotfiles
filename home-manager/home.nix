deabd09f7c06d5011122e4ef21cb619cfec3f8c2
Ryosei Goto <contact@r67018.com>
2025-08-31 22:21:57 +0900

    Rename _1password to _1password-cli because of changing its package name

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -33,9 +33,8 @@ in
     # pkgs.hello
     bat
     seahorse
-    _1password
+    _1password-cli
     _1password-gui
-
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
