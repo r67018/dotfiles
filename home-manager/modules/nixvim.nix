3b177bfc1f0e3353cd740909d87d56d77a22c95f
Ryosei Goto <contact@r67018.com>
2025-09-01 21:54:59 +0900

    Add some plugins in Nixvim

--- home-manager/modules/nixvim.nix
+++ home-manager/modules/nixvim.nix
@@ -6,6 +6,25 @@
     opts = {
       number = true;
       relativenumber = true;
+      autoindent = true;
+      smartindent = true;
+      shiftwidth = 2;
+      tabstop = 2;
+      expandtab = true;
+    };
+    plugins = {
+      nvim-tree = {
+        enable = true;
+      };
+      nvim-autopairs = {
+       enable = true;
+      };
+
+      # Declare plugins below explicitly because they are depended on by other plugins
+      # by nvim-tree
+      web-devicons = {
+        enable = true;
+      };
     };
   };
 }
