9cb2996f56a5f5ba4f611aab0c0548ed98b85f80
Ryosei Goto <contact@r67018.com>
2025-10-28 00:52:29 +0900

    Add tinymist

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -76,6 +76,7 @@ in
 
     dotnet-sdk
     nil # Nix language server
+    tinymist # Typst
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
