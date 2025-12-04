bc92f94a4d0f38ad04cba69c00b2f25c3ca29eb3
Ryosei Goto <contact@r67018.com>
2025-12-05 02:15:24 +0900

    Fix ssh bad owner problem

--- home-manager/modules/ssh.nix
+++ home-manager/modules/ssh.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, lib, ... }:
 let
   onePassPath = "~/.1password/agent.sock";
 in
@@ -8,11 +8,26 @@ in
     enable = true;
     enableDefaultConfig = false;
     includes = [ "config.d/*" ];
-    matchBlocks."*" = {
+    matchBlocks = {
+      "*" = {};
+      "github.com" = {
+        hostname = "github.com";
+        user = "git";
+      };
     };
     extraConfig = ''
         IdentityAgent ${onePassPath}
     '';
   };
+
+  # Fix "Bad owner or permissions on ~/.ssh/config" by converting symlink to file
+  home.activation.fixSshConfigPermissions = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    if [ -L "${config.home.homeDirectory}/.ssh/config" ]; then
+      target=$(readlink "${config.home.homeDirectory}/.ssh/config")
+      rm "${config.home.homeDirectory}/.ssh/config"
+      cp "$target" "${config.home.homeDirectory}/.ssh/config"
+      chmod 600 "${config.home.homeDirectory}/.ssh/config"
+    fi
+  '';
 }
 
