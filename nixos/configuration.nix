d0d6f29dbfbfb9066b3f9d10d80b2830fc1ae185
Ryosei Goto <contact@r67018.com>
2026-01-22 03:47:13 +0900

    Specify user name

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -281,7 +281,7 @@
       enable = true;
       # Certain features, including CLI integration and system authentication support,
       # require enabling PolKit integration on some desktop environments (e.g. Plasma).
-      polkitPolicyOwners = [ "yourUsernameHere" ];
+      polkitPolicyOwners = [ "ryosei" ];
     };
   };
 
