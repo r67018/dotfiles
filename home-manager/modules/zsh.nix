40a0ddc64ade148d6abc8de3dd46b5528ba12fb6
Ryosei Goto <contact@r67018.com>
2026-02-03 20:18:38 +0900

    Add alias in Zsh

--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.zsh = {
     enable = true;
@@ -7,9 +7,19 @@
       grep = "rg";
       vi = "nvim";
       vim = "nvim";
+    } // (if pkgs.stdenv.isDarwin then {
+      ls = "ls -G";
+      ll = "ls -alFG";
+      la = "ls -AG";
+      c = "pbcopy";
+      p = "pbpaste";
+    } else {
+      ls = "ls --color=auto";
+      ll = "ls -alF --color=auto";
+      la = "ls -A --color=auto";
       c = "wl-copy";
       p = "wl-paste";
-    };
+    });
     initContent = ''
       zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
     '';
