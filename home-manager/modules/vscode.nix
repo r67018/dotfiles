dec8328d7af4469b6166d63e37534e17ba44fe03
Ryosei Goto <contact@r67018.com>
2026-07-25 20:04:55 +0900

    home-manager(vscode): use Keychain as password store on Darwin
    The activation script wrote "password-store": "gnome-libsecret" on every
    host, including macOS via hosts/common.nix. There is no gnome-keyring on
    Darwin, so VSCode failed to persist credentials and kept re-prompting for
    authentication. Pick the backend from the host platform instead.

--- home-manager/modules/vscode.nix
+++ home-manager/modules/vscode.nix
@@ -1,20 +1,22 @@
 { pkgs, lib, ... }:
 let
   vscodeArgvFile = "~/.vscode/argv.json";
+  # macOS には gnome-keyring が無いので Keychain を使う
+  passwordStore = if pkgs.stdenv.hostPlatform.isDarwin then "keychain" else "gnome-libsecret";
 in
 {
   programs.vscode.enable = true;
-  # Use gnome-keyring in VSCode:!
+  # Configure VSCode's keyring backend (Linux: gnome-keyring / macOS: Keychain)
   # ref: https://code.visualstudio.com/docs/configure/settings-sync#_recommended-configure-the-keyring-to-use-with-vs-code
   home.activation.vscodeArgs = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     if [ -f ${vscodeArgvFile} ]; then
       # Remove comment in JSON and merge with extra options
       sed -e 's://.*$::' -e '/\/\*/,/\*\//d' ${vscodeArgvFile} | \
-      ${pkgs.jq}/bin/jq '. + {"password-store": "gnome-libsecret"}' > ${vscodeArgvFile}.tmp
+      ${pkgs.jq}/bin/jq '. + {"password-store": "${passwordStore}"}' > ${vscodeArgvFile}.tmp
       mv ${vscodeArgvFile}.tmp ${vscodeArgvFile}
     else
       mkdir -p $(dirname ${vscodeArgvFile})
-      echo '{"password-store": "gnome-libsecret"}' > ${vscodeArgvFile}
+      echo '{"password-store": "${passwordStore}"}' > ${vscodeArgvFile}
     fi
   '';
 }
